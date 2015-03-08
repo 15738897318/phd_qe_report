006ac17ff70b3b5b4b9311baea09fcd1b3e1d158
dreamclinger <chenhaomails@gmail.com>
2015-03-08 22:57:10 +0800

    updated based on the new template

---
+++ NTUQE_Latex_CH/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm *.aux *.bbl *.blg *.dvi *.ps *.bak *.sav *.log *.toc *.lof *.lot *.out
+rm *.synctex.gz
--- NTUQE_Latex_Template/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm *.aux *.bbl *.blg *.dvi *.ps *.bak *.sav *.log *.toc *.lof *.lot *.out
-rm *.synctex.gz
-rm My_QE_Report.pdf
